06c5b0a0314a9d08ddb94dc51fd5045d3511e400
Colin Germain <cgermain@gmail.com>
2017-01-08 16:35:05 -0500

    Cleaned up formatting.

--- flaskTest.py
+++ flaskTest.py
@@ -3,7 +3,6 @@ from functools import update_wrapper
 app = Flask(__name__)
 from time import time
 from SCRIPTS_FOR_GUI import mgf_select_one
-# from SCRIPTS_FOR_GUI import helper
 import json
 from os.path import join
 from SCRIPTS_FOR_GUI import combine_selected_mgf_files
@@ -14,14 +13,11 @@ import math
 from SCRIPTS_FOR_GUI import utility
 from SCRIPTS_FOR_GUI import validation
 from SCRIPTS_FOR_GUI import handle_inverse_posts
-# from copy import copy
-# from science_code import science
 from SCRIPTS_FOR_GUI import makeFolderNames
 import shutil
 from datetime import datetime
 import pandas as pd
 import re
-
 
 TIME_FORMAT =  "%m-%d-%Y_%H-%M-%S"
 
@@ -36,13 +32,10 @@ def nocache(f):
 @nocache
 def main():
 	if not utility.inverse_array_is_correct():
-		# return "<div>OH HELLO</div>"
 		true_inverse_files = utility.get_true_inverse_array()
 		inverse_file_array = utility.get_inverse_files_array()
 		return render_template('bad_inverse_names.html', true_inverse_files=true_inverse_files,\
 			inverse_file_array=inverse_file_array)
-	# Right now, the m,ain thing that might mess up is, if someone
-	# messed with the names of the inverse files.
 
 	return render_template('index_new.html')
 
@@ -58,21 +51,15 @@ def tab():
 
 @app.route("/tab_4_helper_function", methods=['POST'])
 def tab_4_helper_function():
-	print "tab 4 helper function"
-
 	valid, validation_error = validation.validate_tab_4(request.form)
 	if not valid:
 		return validation_error, 500
 
-	#find where this rename goes!
-	#mgf_write_dir_path = "renameme"
 	mgf_txt_write_dir_path = makeFolderNames.construct_plain_parse_reporter_folder_path(request.form)
 	mgf_file_name = request.form['mgfFileName']
 	mgf_read_dir_path = request.form['mgfReadDirPath']
 	mgf_txt_write_path = join(mgf_txt_write_dir_path, mgf_file_name.split('.mgf')[0] + '.reporter')
-	#mgf_write_path = join(mgf_write_dir_path, mgf_file_name)
 	mgf_read_path = join(mgf_read_dir_path, mgf_file_name)
-
 
 	try:
 		os.makedirs(mgf_txt_write_dir_path)
@@ -99,25 +86,19 @@ def plain_parse_xtandem_combine_with_mgf():
 
 	mgf_txt_foldername = makeFolderNames.construct_plain_parse_reporter_folder_path(request.form)
 
-	# print "going to call parse_xtandem stuff from tab 5"
-
 	a = call_xml_parser.plain_parse_xtandem_combine_with_mgf(xml_read_path, log_error_threshold, geneFile, mgf_txt_foldername, unacceptable_mods)
 
 	if a:
 		print "Error in tab 4. Trying cleanup now, either way returning error"
 		try:
-			#todo fix this
 			clean_up_after_tab_2()
 		finally: #In case it breaks
 			return a, 500
 	else:
 		return "Looks good"
 
-
 @app.route("/tab_2_helper_function", methods=['POST'])
 def tab_2_helper_function():
-	# print request.form
-	print "tab 2 helper function"
 	valid, validation_error = validation.validate_tab_2(request.form)
 	if not valid:
 		return validation_error, 500
@@ -131,12 +112,9 @@ def tab_2_helper_function():
 	normalize_intensities = request.form.getlist('normalizeIntensities')
 
 	if request.form['mgfOperationToPerform'] == '1':
-		print "Looks like we had to select from the mgf folder before this, that means I'll recalculate the mgf_foldername"
-		# mgf_txt_foldername = join(request.form['mgfReadDirPath'], 'selected_mgf_txt', '')
 		mgf_txt_foldername = makeFolderNames.construct_reporter_folder_path(request.form)
 	else:
 		mgf_txt_foldername = request.form["mgfTxtReadDirPath"]
-		print "Checking if previous summary exists"
 		previous_ion_type = check_if_previous_summary_exists_and_get_reporter_type(mgf_txt_foldername)
 		if not previous_ion_type:
 			return "No previous summary found.  Please reselect reporters and keep the summary.", 500
@@ -149,7 +127,7 @@ def tab_2_helper_function():
 	a = call_xml_parser.parse_xtandem_combine_with_mgf(xml_read_path, log_error_threshold, reporter_type, geneFile, mgf_txt_foldername, unacceptable_mods, normalize_intensities)
 
 	if a:
-		print "Error in tab 2. Cleaning up"
+		print "Error in tab 2. Cleaning up."
 		try:
 			clean_up_after_tab_2()
 		finally: #In case it breaks
@@ -157,24 +135,12 @@ def tab_2_helper_function():
 	else:
 		return "Looks good"
 
-
-
 @app.route("/tab_1_helper_function", methods=['POST'])
 def tab_1_helper_function():
-	# print request.form
 	valid, validation_error = validation.validate_tab_1(request.form)
-	# print valid
-	# print validation_error
 	if not valid:
-		print "not valid! error!"
-		print "Not valid, error is " + str(validation_error)
+		print "Not valid: error is " + str(validation_error)
 		return validation_error, 500
-	# else:
-	# 	print "validation passed, this is being printed inside of tab_1_helper_function"
-	# Wow,  that's so much better.
-	# Now, to make the values.
-
-	# I'm pretty confident about this, so I'll move on for now.
 
 	mgf_read_dir_path = request.form['mgfReadDirPath']
 	mgf_file_name = request.form['mgfFileName']
@@ -190,9 +156,7 @@ def tab_1_helper_function():
 	mz_error_initial_run = request.form['mzErrorInitialRun']
 	mz_error_recalibration = request.form['mzErrorRecalibration']
 
-	#Now, to check/make directories 
-	# print "accessed all of the variables"
-
+	#Check/make directories 
 	if should_select == "1":
 		mgf_write_dir_path = makeFolderNames.construct_selected_mgf_path(request.form)
 		mgf_write_path = join(mgf_write_dir_path, mgf_file_name)
@@ -212,7 +176,6 @@ def tab_1_helper_function():
 	if not os.path.isdir(mgf_txt_write_dir_path):
 		return "selected_mgf_txt directory could not be created", 500
 
-	# print "made a directory maybe"
 	if should_select == '1':
 		try:
 			os.makedirs(mgf_write_dir_path)
@@ -220,10 +183,6 @@ def tab_1_helper_function():
 			print "mgf directory probably already there"
 		if not os.path.isdir(mgf_write_dir_path):
 			return "selected_mgf directory could not be created", 500	
-
-		# Still ugly, but that's because it's complicated
-	# print "maybe made another"
-
 
 	if perform_recalibration == '1':
 		print "calling perform_recalibration from 1"
@@ -239,7 +198,7 @@ def tab_1_helper_function():
 
 	else:
 		print "calling mgf_select_no_recalibrate from 1"
-		# I can do this because I checked for both in validation
+		# Can do this because both were validated
 		error = mgf_select_one.select_only_one(mgf_read_path, \
 			mgf_write_path, mgf_txt_write_path, mz_error, reporter_type, \
 			min_intensity, min_reporters, should_select)
@@ -274,8 +233,6 @@ def check_if_gpm_merge_already_exists():
 		print "error creating the foldername. At least that means it doesn't exist"
 		return "Does not exist already" #That means true
 
-
-
 @app.route("/getMGFFiles", methods=['POST'])
 def getMGFFiles():
 	try:
@@ -287,7 +244,6 @@ def getMGFFiles():
 		return text
 	except:
 		return "Error selecting mgf files, make sure you have a proper mgf directory name", 500
-
 
 @app.route("/createInverseFiles", methods=['POST'])
 @nocache
@@ -303,7 +259,6 @@ def createInverseFiles():
 @app.route("/writeSummary", methods=['POST'])
 @nocache
 def writeSummary():
-	#print request.form
 	timestamp = datetime.now().strftime(TIME_FORMAT)
 	if request.form['mgfOperationToPerform'] == '1':
 		if 'plain_parse' in request.form and request.form['plain_parse'] == "1":
@@ -337,8 +292,8 @@ def writeSummary():
 			os.remove(mgf_txt_write_dir_path+'intensity_summary.txt')
 
 	makeFolderNames.rename_folders(request.form)
+	print "Summary compelete."
 	return "Summary complete."
-
 
 def clean_up_after_tab_2():
 	print "cleaning up possible leaked files, if there was an error somewhere."
@@ -347,31 +302,27 @@ def clean_up_after_tab_2():
 	# testing_filename = mgf_txt_filename.split('.reporter')[0] + '_duplicate_sorted' + '.reporter'
 	# is another.
 	xml_read_path = request.form['xmlReadPath']
-	print "xml_read_path: " + xml_read_path
 	xml_directory_path = utility.xml_dirname_from_filename(xml_read_path)
 	if not xml_directory_path:
-		print "Something funky, stop for now"
+		print "Problem with XML directory while cleaning tab 2"
 		return
 	if (os.path.isfile(xml_read_path + '.txt')):
 		os.remove(xml_read_path + '.txt')
 
 	if (os.path.isdir(xml_directory_path)):
-		# I REALLY want to make sure this doesn't delete everything on somebodys computer.
-		# Done, in xml_dirname_from_filename
 		try:
 			shutil.rmtree(xml_directory_path)
 		except Exception:
-			print "no luck, probably read-only files or something. There's nothing we can do about that."
-	print "cleaned up xmldir, now for the temporary files in .reporter directory"
+			print "Error removing all files.  Possibly read-only files in folder."
+
 	mgf_txt_foldername = None
 	if request.form['mgfOperationToPerform'] == '1':
-		print "Looks like we had to select from the mgf folder before this, that means I'll recalculate the mgf_foldername"
-		# mgf_txt_foldername = join(request.form['mgfReadDirPath'], 'selected_mgf_txt', '')
+		print "Recalculating MGF folder name"
 		mgf_txt_foldername = makeFolderNames.construct_reporter_folder_path(request.form)
 	else:
 		mgf_txt_foldername = request.form["mgfTxtReadDirPath"]
 	if not mgf_txt_foldername:
-		print "Something is wrong here, but anyways, nothing to delete"
+		print "MGF txt folder missing.  Nothing to delete."
 		return
 
 	for item in os.listdir(mgf_txt_foldername):
@@ -387,14 +338,10 @@ def clean_up_after_tab_2():
 			if full_name.ends_with('_PLACEHOLDER'):
 				os.remove(full_name)
 				continue
-			#TODO clean up intensity summary if it fails
 			if full_name.ends_with('intensity_summary.txt'):
 				os.remove(full_name)
 				continue
-
-	print "Cleaned up successfully"
 	return
-
 
 def check_if_previous_summary_exists_and_get_reporter_type(reporter_folder):
 	ion_type = ""
@@ -404,7 +351,6 @@ def check_if_previous_summary_exists_and_get_reporter_type(reporter_folder):
 			with open(reporter_folder+"\\"+item, "r") as summary:
 				with open(reporter_folder+"\intensity_summary.txt","w") as intensity_summary:
 					for line in summary:
-						print line
 						ion_type_search = re.search("reporterIonType - (.*)", line)
 						if ion_type_search:
 							ion_type = ion_type_search.group(1)
@@ -464,7 +410,6 @@ def return_form_copy():
 		to_return[key] = request.form[key]
 	return to_return	
 
-
 def multiple_select_to_two_arrays(unacceptable_mods):
 	long_string = ','.join(unacceptable_mods)
 	good_array = long_string.split(',')
@@ -476,7 +421,3 @@ def multiple_select_to_two_arrays(unacceptable_mods):
 if __name__ == "__main__":
 	app.debug = True
 	app.run()
-  # app.run(processes=8, debug=True)
-  
-  # app.run()
-
